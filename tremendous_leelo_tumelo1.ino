76724d2f6ee1cc121b97935b7c11d6c7341864fc
justasn88 <justasn88@gmail.com>
2025-09-29 18:55:53 +0300

    Update tremendous_leelo_tumelo1.ino

--- tremendous_leelo_tumelo1.ino
+++ tremendous_leelo_tumelo1.ino
@@ -1,20 +1,55 @@
-int sensorPin = A0;   
-int motorPin = 9;     
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x20, 16, 2);
+
+int TmpSensorPin = A0;    
+int motorPin = 9;       
+int HumSensorPin = A1;
 
 float voltage, temperatureC;
+int fanSpeed;
 
 void setup() {
+  lcd.init();
+  lcd.backlight();
   pinMode(motorPin, OUTPUT);
-  Serial.begin(1000);
+  Serial.begin(9600);
+
 }
 
 void loop() {
-  int sensorValue = analogRead(sensorPin);
+  int TmpSensorValue = analogRead(TmpSensorPin);
+  int HumSensorValue = analogRead(HumSensorPin);
   
-  voltage = sensorValue * (5.0 / 1023.0);
-  temperatureC = (voltage - 0.5) * 100.0; 
+  voltage = TmpSensorValue * (5.0 / 1023.0);
+  temperatureC = (voltage - 0.5) * 100.0;  
   
-  int fanSpeed = map(temperatureC, 24, 50, 0, 255);
+  int humidityPercentage = map(HumSensorValue, 0, 1023, 10, 70);
+  
+  int baseFanSpeed = map(temperatureC, 24, 50, 0, 255);
+  
+  if (humidityPercentage > 50) {
+    int humidityBoost = map(humidityPercentage, 50, 79, 0, 100);
+    fanSpeed = baseFanSpeed + humidityBoost;
+  }
+  else {
+    fanSpeed = baseFanSpeed;
+  }
+  
+  lcd.clear();
+  
   fanSpeed = constrain(fanSpeed, 0, 255);
   analogWrite(motorPin, fanSpeed);
+  
+  lcd.setCursor(0, 0); 
+  lcd.print("Temp: ");
+  lcd.print(temperatureC);
+  lcd.print(" C");
+
+  lcd.setCursor(0, 1); 
+  lcd.print("Dregme: ");
+  lcd.print(humidityPercentage);
+  lcd.print(" %");
+  
+  delay(1000);
 }
